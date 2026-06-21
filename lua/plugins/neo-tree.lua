57d8dec79b5b3d5886ed301d6d1d8f2558e96182
Thanh Tran Xuan <thanhtranxuan.dev@gmail.com>
2026-06-21 20:47:49 +0700

    fix(neotree): toggle neotree for mini.starter buffer

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,17 +1,23 @@
-vim.pack.add {
-    { src = 'https://github.com/nvim-neo-tree/neo-tree.nvim', version = vim.version.range '*' },
-    'https://github.com/nvim-lua/plenary.nvim',
-    'https://github.com/MunifTanjim/nui.nvim',
-}
+vim.pack.add({
+	{ src = "https://github.com/nvim-neo-tree/neo-tree.nvim", version = vim.version.range("*") },
+	"https://github.com/nvim-lua/plenary.nvim",
+	"https://github.com/MunifTanjim/nui.nvim",
+})
 
-vim.keymap.set('n', '\\', '<Cmd>Neotree reveal<CR>', { desc = 'NeoTree reveal', silent = true })
+vim.keymap.set("n", "\\", function()
+	if vim.bo.filetype == "ministarter" then
+		vim.cmd("Neotree toggle filesystem")
+	else
+		vim.cmd("Neotree reveal")
+	end
+end, { desc = "NeoTree toggle", silent = true })
 
-require('neo-tree').setup {
-    filesystem = {
-        window = {
-            mappings = {
-                ['\\'] = 'close_window',
-            },
-        },
-    },
-}
+require("neo-tree").setup({
+	filesystem = {
+		window = {
+			mappings = {
+				["\\"] = "close_window",
+			},
+		},
+	},
+})
